2d73d617a4448b11194ded205575f858fefbb315
Leandro Souza <leandro.costa@ifba.edu.br>
2023-09-01 03:38:06 +0000

    first commit

---
+++ .vuepress/config.ts
@@ -0,0 +1,24 @@
+import { defineUserConfig } from "vuepress";
+import { searchPlugin } from "@vuepress/plugin-search";
+import umlPlugin from 'markdown-it-plantuml';
+import theme from "./theme.js";
+
+export default defineUserConfig({
+  base : "/blog-material-aula/",
+
+  lang: "pt-BR",
+  title: "20232 PWEB Blog das Aulas",
+  description: "Material das Aulas",
+
+  extendsMarkdown: (md) => {    
+    md.use(umlPlugin, {openMarker: '```plantuml', closeMarker: '```'})
+  },
+
+  theme,
+
+  // Enable it with pwa
+  shouldPrefetch: false,
+
+  plugins: [searchPlugin({}),],
+
+});
